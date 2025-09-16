c4ec1ab314df0cba80f2040d585fb70ce7c5113f
Chris Porter <chris@uprightvinyl.co.uk>
2025-09-17 07:12:39 +1000

    correcting github URL

--- adopt-the-cloud-deployVMs.ps1
+++ adopt-the-cloud-deployVMs.ps1
@@ -1,4 +1,4 @@
-git clone https://github.com/uprightvinyl/adopt-the-cloud/adopt-the-cloud.git # Exlude this step if you have already cloned the repo
+git clone https://github.com/uprightvinyl/adopt-the-cloud.git # Exlude this step if you have already cloned the repo
 Invoke-WebRequest -outfile "install-opentofu.ps1" -uri "https://get.opentofu.org/install-opentofu.ps1"
 .\install-opentofu.ps1 -installmethod standalone -skipVerify
 cd adopt-the-cloud
