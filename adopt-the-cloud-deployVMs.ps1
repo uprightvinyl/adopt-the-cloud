0bd43447c52cc27f1af96bb6de51b4900b48f9ee
Chris Porter <chris@uprightvinyl.co.uk>
2025-09-17 07:05:12 +1000

    reordering deploy file

--- adopt-the-cloud-deployVMs.ps1
+++ adopt-the-cloud-deployVMs.ps1
@@ -1,7 +1,6 @@
-
+git clone https://github.com/uprightvinyl/adopt-the-cloud/adopt-the-cloud.git # Exlude this step if you have already cloned the repo
 Invoke-WebRequest -outfile "install-opentofu.ps1" -uri "https://get.opentofu.org/install-opentofu.ps1"
 .\install-opentofu.ps1 -installmethod standalone -skipVerify
-git clone https://github.com/uprightvinyl/adopt-the-cloud/adopt-the-cloud.git
 cd adopt-the-cloud
 tofu init
 tofu apply -auto-approve
